dff9a2511095ba56f44d7f37072a84ecc893cd54
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-08-24 00:27:29 +0900

    feat(@lemma/http): add oauth2 authorize initiation, without nonce

--- packages/lemma__http/src/auth/routes.ts
+++ packages/lemma__http/src/auth/routes.ts
@@ -2,8 +2,15 @@ import { FastifyInstance } from 'fastify';
 
 export default async function routes(fastify: FastifyInstance) {
   fastify.get('/google', async (request, reply) => {
-    reply.statusCode = 501;
-    reply.send('Not implemented');
+    const oauthAuthorizeUrl = new URL('https://accounts.google.com/o/oauth2/v2/auth');
+
+    oauthAuthorizeUrl.searchParams.set('response_type', 'code');
+    oauthAuthorizeUrl.searchParams.set('client_id', fastify.env.auth.providers.google.clientId);
+    oauthAuthorizeUrl.searchParams.set('scope', 'openid profile email');
+    oauthAuthorizeUrl.searchParams.set('redirect_uri', fastify.env.auth.providers.google.redirectUrl);
+    // @todo state
+
+    reply.redirect(302, oauthAuthorizeUrl.href);
   });
 
   fastify.get('/google/redirect', async (request, reply) => {
