c3e0e528fff0c0efc01f282aefbdf0c1e4917ac6
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-08-24 00:42:54 +0900

    feat(@lemma/http): generate nonce and store the nonce at the requested session

--- packages/lemma__http/src/auth/routes.ts
+++ packages/lemma__http/src/auth/routes.ts
@@ -1,14 +1,23 @@
+import crypto from 'crypto';
 import { FastifyInstance } from 'fastify';
+
+function nonce() {
+  return crypto.randomBytes(32).toString('hex');
+}
 
 export default async function routes(fastify: FastifyInstance) {
   fastify.get('/google', async (request, reply) => {
     const oauthAuthorizeUrl = new URL('https://accounts.google.com/o/oauth2/v2/auth');
 
+    const state = nonce();
+
     oauthAuthorizeUrl.searchParams.set('response_type', 'code');
     oauthAuthorizeUrl.searchParams.set('client_id', fastify.env.auth.providers.google.clientId);
     oauthAuthorizeUrl.searchParams.set('scope', 'openid profile email');
     oauthAuthorizeUrl.searchParams.set('redirect_uri', fastify.env.auth.providers.google.redirectUrl);
-    // @todo state
+    oauthAuthorizeUrl.searchParams.set('state', state);
+
+    request.session.set('state', state);
 
     reply.redirect(302, oauthAuthorizeUrl.href);
   });
