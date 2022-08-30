0d9ee4767239fbbd0edf5b90852d843c128f29cd
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-08-30 20:38:28 +0900

    feat(@lemma/http): add protected route plugin, implement GET /account/me

--- packages/lemma__http/src/account/routes.ts
+++ packages/lemma__http/src/account/routes.ts
@@ -1,7 +1,26 @@
 import { FastifyInstance } from 'fastify';
+import sessionGuard from '~/lib/session-guard';
 
 export default async function routes(fastify: FastifyInstance) {
-  fastify.get('/me', async (request, reply) => {
+  fastify.register(sessionGuard);
 
+  fastify.get('/me', async (request, reply) => {
+    const account = await fastify.rdb.account.findUnique({
+      where: { id: request.session.accountId },
+    });
+
+    if (!account) {
+      return reply
+        .status(404)
+        .send({ statusCode: 404, message: 'Not Found' });
+    }
+
+    return { account };
+  });
+
+  fastify.delete('/logout', async (request, reply) => {
+    return reply
+      .status(501)
+      .send({ statusCode: 501, message: 'Not Implemented' });
   });
 }
---
+++ packages/lemma__http/src/lib/session-guard.ts
@@ -0,0 +1,14 @@
+import { FastifyInstance } from 'fastify';
+import fp from 'fastify-plugin';
+
+async function sessionGuard(fastify: FastifyInstance) {
+  fastify.addHook('preHandler', async (request, reply) => {
+    if (!request.session.accountId) {
+      return reply
+        .status(401)
+        .send({ statusCode: 401, message: 'Unauthorized' });
+    }
+  });
+}
+
+export default fp(sessionGuard);
