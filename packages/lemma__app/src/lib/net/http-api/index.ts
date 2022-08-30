eb4ded6f1c52190937cd7f314c844bd044c405fc
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-08-31 00:53:35 +0900

    feat(@lemma/app): support search params in HttpApi.url

--- packages/lemma__app/src/lib/net/http-api/index.ts
+++ packages/lemma__app/src/lib/net/http-api/index.ts
@@ -1,5 +1,11 @@
 import { Env } from '~/lib/env';
 
 export namespace HttpApi {
-  export const url = (path: string): string => `${Env.httpApiHost}${path}`;
+  export const url = (path: string, params?: URLSearchParams): string => {
+    const url = new URL(path, Env.httpApiHost);
+    if (params) {
+      url.search = params.toString();
+    }
+    return url.toString();
+  };
 }
