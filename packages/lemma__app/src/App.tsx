8589d49828622e2bdee044db25d0f66d04606ec2
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-09-04 00:31:14 +0900

    feat(@lemma/app): setup query client provider

--- packages/lemma__app/src/App.tsx
+++ packages/lemma__app/src/App.tsx
@@ -1,13 +1,18 @@
 import { LightFoundation, useFoundation } from '@channel.io/bezier-react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { FoundationProvider } from '~/lib/foundation';
 import GlobalStyle from './global-style';
 import Routes from './Routes';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <FoundationProvider>
-      <GlobalStyle foundation={useFoundation() ?? LightFoundation} />
-      <Routes />
-    </FoundationProvider>
+    <QueryClientProvider client={queryClient}>
+      <FoundationProvider>
+        <GlobalStyle foundation={useFoundation() ?? LightFoundation} />
+        <Routes />
+      </FoundationProvider>
+    </QueryClientProvider>
   );
 }
