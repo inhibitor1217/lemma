a4140ce07b4cfbf59e8755920e08c954ced50df8
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-09-04 12:38:48 +0900

    feat(@lemma/app): add temporary loading component for App

--- packages/lemma__app/src/page/app/index.tsx
+++ packages/lemma__app/src/page/app/index.tsx
@@ -1,11 +1,28 @@
 import { Navigate } from 'react-router-dom';
 import { withAuth } from '~/lib/auth';
+import { text } from '~/lib/i18n';
+import { Center } from '~/lib/layout';
+import { FullscreenPage } from '~/lib/page-template';
+import { Text, Typography } from '~/lib/typography';
 import { InternalPath } from '../path';
 
 function App() {
   return <div />;
 }
 
+function AppLoading() {
+  return (
+    <FullscreenPage>
+      <Center>
+        <Text as="h3" typo={Typography.Size18} color="txt-black-darker">
+          {text('Loading ...')}
+        </Text>
+      </Center>
+    </FullscreenPage>
+  );
+}
+
 export default withAuth(App, {
+  Loading: AppLoading,
   Error: () => <Navigate to={InternalPath.Authorize} />,
 });
