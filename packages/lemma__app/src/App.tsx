a436b50639dac4f7552342a344ac29eaf2fb5424
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-09-04 22:41:58 +0900

    feat(@lemma/app): setup recoil

--- packages/lemma__app/src/App.tsx
+++ packages/lemma__app/src/App.tsx
@@ -1,18 +1,21 @@
 import { FoundationProvider } from '~/lib/foundation';
 import { LayoutProvider } from '~/lib/layout';
 import { ReactQueryProvider } from '~/lib/react-query';
+import { RecoilProvider } from '~/lib/recoil';
 import GlobalStyle from './global-style';
 import Routes from './Routes';
 
 export default function App() {
   return (
     <ReactQueryProvider>
-      <FoundationProvider>
-        <LayoutProvider>
-          <GlobalStyle />
-          <Routes />
-        </LayoutProvider>
-      </FoundationProvider>
+      <RecoilProvider>
+        <FoundationProvider>
+          <LayoutProvider>
+            <GlobalStyle />
+            <Routes />
+          </LayoutProvider>
+        </FoundationProvider>
+      </RecoilProvider>
     </ReactQueryProvider>
   );
 }
---
+++ packages/lemma__app/src/lib/recoil/RecoilProvider.tsx
@@ -0,0 +1,6 @@
+import { PropsWithChildren } from 'react';
+import { RecoilRoot } from 'recoil';
+
+export default function RecoilProvider({ children }: PropsWithChildren<{}>) {
+  return <RecoilRoot>{children}</RecoilRoot>;
+}
