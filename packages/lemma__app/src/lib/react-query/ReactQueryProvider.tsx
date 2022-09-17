c357ab1e35878ad578261c91535801eb403236d1
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-09-17 19:09:21 +0900

    fix(@lemma/app): move devtools ui launcher to bottom-right

--- packages/lemma__app/src/lib/react-query/ReactQueryProvider.tsx
+++ packages/lemma__app/src/lib/react-query/ReactQueryProvider.tsx
@@ -8,7 +8,7 @@ export default function ReactQueryProvider({ children }: PropsWithChildren<{}>) 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
 }
