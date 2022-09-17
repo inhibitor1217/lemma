e4c8e8b57c01c30b2f5b5eb7bd6dddee169268e3
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-09-17 21:17:38 +0900

    fix(@lemma/http): refresh session expiration when session guard is passed
    * update @fastify/session to v10, which fixes request.session.touch() to work properly

--- packages/lemma__http/src/lib/session-guard.ts
+++ packages/lemma__http/src/lib/session-guard.ts
@@ -4,10 +4,10 @@ import fp from 'fastify-plugin';
 async function sessionGuard(fastify: FastifyInstance) {
   fastify.addHook('preHandler', async (request, reply) => {
     if (!request.session.accountId) {
-      return reply
-        .status(401)
-        .send({ statusCode: 401, message: 'Unauthorized' });
+      return reply.status(401).send({ statusCode: 401, message: 'Unauthorized' });
     }
+
+    request.session.touch();
   });
 }
 
