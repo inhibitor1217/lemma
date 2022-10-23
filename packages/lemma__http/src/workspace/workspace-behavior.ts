35e99d6afe81ae841737bca5e367e4ee65e4c1e9
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-10-23 16:02:08 +0900

    feat(@lemma/http): implement workspaceBehavior

--- packages/lemma__http/src/workspace/workspace-behavior.ts
+++ packages/lemma__http/src/workspace/workspace-behavior.ts
@@ -12,12 +12,31 @@ declare module 'fastify' {
 }
 
 export async function workspaceBehavior(fastify: FastifyInstance) {
-  async function getWorkspacesPage(accountId: Account['id'], options: OffsetPagination.RequestOptions) {
-    return [];
+  async function getWorkspacesPage(accountId: Account['id'], options: OffsetPagination.RequestOptions): Promise<Workspace[]> {
+    return fastify.rdb.workspace.findMany({
+      where: {
+        members: {
+          some: {
+            accountId,
+          },
+        },
+      },
+      ...OffsetPagination.optionsToRdbQuery(options),
+    });
   }
 
-  async function getWorkspacesNumPages(accountId: Account['id']) {
-    return OffsetPagination.toNumPages(0);
+  async function getWorkspacesNumPages(accountId: Account['id']): Promise<number> {
+    return fastify.rdb.workspace
+      .count({
+        where: {
+          members: {
+            some: {
+              accountId,
+            },
+          },
+        },
+      })
+      .then(OffsetPagination.toNumPages);
   }
 
   fastify.decorate('workspaceBehavior', {
