2431ab6b8f66d28e5f26caf03deb923e7a07e9e0
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-10-23 15:03:38 +0900

    feat(@lemma/http): log raw queries from prisma ORM

--- packages/lemma__http/src/db/rdb.ts
+++ packages/lemma__http/src/db/rdb.ts
@@ -4,14 +4,30 @@ import fp from 'fastify-plugin';
 
 declare module 'fastify' {
   interface FastifyInstance {
-    rdb: PrismaClient
+    rdb: typeof rdbClient;
   }
 }
 
-const rdbClient = new PrismaClient();
+const rdbClient = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+  ],
+});
+
+async function queryLogger(fastify: FastifyInstance) {
+  fastify.rdb.$on('query', (e) => {
+    fastify.log.debug(`[RDB:query] ${e.query}`);
+    fastify.log.debug(`[RDB:params] ${e.params}`);
+    fastify.log.debug(`[RDB:duration] ${e.duration}ms`);
+  });
+}
 
 async function rdb(fastify: FastifyInstance) {
   fastify.decorate('rdb', rdbClient);
+  fastify.register(queryLogger);
 }
 
 export default fp(rdb);
