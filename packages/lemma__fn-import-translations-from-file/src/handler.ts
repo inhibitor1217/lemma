13e1286f97e648c3bb3e2a6e25db0fa093fe627e
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-12-18 23:15:03 +0900

    feat(fn-import-translations-from-file): add file existence check

--- packages/lemma__fn-import-translations-from-file/src/handler.ts
+++ packages/lemma__fn-import-translations-from-file/src/handler.ts
@@ -1,24 +1,46 @@
 import { AWSS3Client, AWSS3ClientArgs } from '@lemma/aws-s3';
-import { FileStorageClient } from '@lemma/file-storage-client';
+import { FileStorageClient, FileStorageLocation } from '@lemma/file-storage-client';
 import { Handler } from 'aws-lambda';
 import { Event, Result } from './types';
 
 const s3 = new AWSS3Client(
-  process.env.STAGE === 'local'
+  process.env.STAGE === 'dev'
     ? AWSS3ClientArgs.customEndpoint({
         endpoint: process.env.AWS_ENDPOINT || '',
+        region: process.env.AWS_REGION || '',
         resourcePrefix: process.env.AWS_RESOURCE_PREFIX || '',
         logger: console,
       })
     : AWSS3ClientArgs.region({
-        region: process.env.AWS_REGION || '',
         resourcePrefix: process.env.AWS_RESOURCE_PREFIX || '',
+        region: process.env.AWS_REGION || '',
         logger: console,
       })
 );
 
 const fileStorage = new FileStorageClient(s3);
 
-export const handler: Handler<Event, Result> = (event, context) => {
-  return Promise.resolve({});
+class TranslationsFileNotFoundException extends Error {
+  constructor() {
+    super('TranslationsFileNotFoundException');
+  }
+}
+
+export const handler: Handler<Event, Result> = async (event, context) => {
+  const {
+    payload: {
+      workspaceId,
+      translationsImportAttemptId,
+      file: { type, key },
+      translations: { language },
+    },
+  } = event;
+
+  if (!(await fileStorage.exists(FileStorageLocation.Internal, key))) {
+    throw new TranslationsFileNotFoundException();
+  }
+
+  console.log(key);
+
+  return {};
 };
