50d9fe6cacdec6b3ca27934ef7194c6373d30a1f
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-12-18 22:40:31 +0900

    feat(file-storage-client): implement FileStorageClient#exists

--- packages/lemma__file-storage-client/src/file-storage-client.ts
+++ packages/lemma__file-storage-client/src/file-storage-client.ts
@@ -2,10 +2,15 @@ import { Either } from '@lemma/fx';
 import { FileStorageLocation } from './file-storage-location';
 
 interface StorageClient {
+  headObject(bucketName: string, key: string): Promise<Either<unknown, unknown>>;
   putObject(bucketName: string, key: string, file: Blob | Buffer | ReadableStream): Promise<Either<{ httpUri: string }, unknown>>;
 }
 
 export namespace FileStorageClient {
+  export type ExistsResult = boolean;
+
+  export type ExistsError = { message: 'NoSuchKeyException' } | unknown;
+
   export type UploadFileResult = {
     httpUri: string;
   };
@@ -21,6 +26,27 @@ export class FileStorageClient {
    * to support multiple vendors.
    */
   constructor(private readonly storage: StorageClient) {}
+
+  public exists(
+    location: FileStorageLocation,
+    key: string
+  ): Promise<Either<FileStorageClient.ExistsResult, FileStorageClient.ExistsError>> {
+    return this.storage
+      .headObject(location.bucketName, key)
+      .then(
+        Either.reduce(
+          () => Either.ok(true),
+          (e) => {
+            if (e instanceof Error && e.message.includes('NoSuchKeyException')) {
+              return Either.ok(false);
+            }
+
+            return Either.error(e);
+          }
+        )
+      )
+      .catch(Either.error);
+  }
 
   public uploadFile(
     location: FileStorageLocation,
