9e79041fa0004d00118be0836b7e0882952f04bf
inhibitor1217 <inhibitor@kaist.ac.kr>
2022-12-18 12:50:53 +0900

    feat(fn-import-translations-from-file): add dependency from aws-s3, file-storage-client

--- packages/lemma__fn-import-translations-from-file/src/handler.ts
+++ packages/lemma__fn-import-translations-from-file/src/handler.ts
@@ -1,5 +1,23 @@
+import { AWSS3Client, AWSS3ClientArgs } from '@lemma/aws-s3';
+import { FileStorageClient } from '@lemma/file-storage-client';
 import { Handler } from 'aws-lambda';
 import { Event, Result } from './types';
+
+const s3 = new AWSS3Client(
+  process.env.STAGE === 'local'
+    ? AWSS3ClientArgs.customEndpoint({
+        endpoint: process.env.AWS_ENDPOINT || '',
+        resourcePrefix: process.env.AWS_RESOURCE_PREFIX || '',
+        logger: console,
+      })
+    : AWSS3ClientArgs.region({
+        region: process.env.AWS_REGION || '',
+        resourcePrefix: process.env.AWS_RESOURCE_PREFIX || '',
+        logger: console,
+      })
+);
+
+const fileStorage = new FileStorageClient(s3);
 
 export const handler: Handler<Event, Result> = (event, context) => {
   return Promise.resolve({});
